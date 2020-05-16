5b85c9f36907d89cdd8eb4f54a4547bed05c8292
lg0017 <luizmb16@gmail.com>
2020-05-15 23:52:08 -0300

    Corrigindo erros gerais

--- src/main.c
+++ src/main.c
@@ -1,10 +1,3 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
 #include <stdlib.h> /* para utilizar exit */
 #include <sys/types.h> /* para utilizar pid_t */
@@ -16,38 +9,46 @@
 int primo(int n){
 	int aux = 2;
 	int verificacao = 1;
-
-    if (n <= 1){
+	
+	if (n <= 1){
 		verificacao = 0;
 	}
-
-    while (verificacao == 1 && aux <= n / 2) {
+	
+    	while (verificacao == 1 && aux <= n / 2) {
 		if (n % aux  == 0){
 			verificacao = 0;
 		}
 		aux = aux + 1;
-    }
-
+    	}
+	
 	return verificacao;
 }
-
+	
 int main() {
-	pid_t filho[3];
+	pid_t filho[4];
+
 	int entrada[100];
 	int qtd = 0, i = 0, j = 0, k = 0, saida = 0, inteiro;
 	char caracter;
-
+	
 	/* Definir flags de protecao e visibilidade de memoria */
 	int protection = PROT_READ | PROT_WRITE;
 	int visibility = MAP_SHARED | MAP_ANON;
 
-	/* Criar area de memoria compartilhada */
-	int *contador; /* Variavel contadora visivel para todos os processos */
+	int *contador; 
 	contador = (int*) mmap(NULL, sizeof(int)*100, protection, visibility, 0, 0);
-	int *numero; /* Variavel contadora visivel para todos os processos */
+	int *numero; 
 	numero = (int*) mmap(NULL, sizeof(int)*100, protection, visibility, 0, 0);
+	int *libera; 
+	libera = (int*) mmap(NULL, sizeof(int)*4, protection, visibility, 0, 0);
 
-	 while (saida != -1){
+	for(i = 0; i < 4; i++){
+		libera[i] = 0;
+	}
+
+	i = 0;
+	
+	while (saida != -1){
 		scanf("%d%c", &inteiro, &caracter);
 		if(caracter == '\n'){
 			entrada[i] = inteiro;
@@ -58,33 +59,34 @@ int main() {
 			i++;
 			qtd++;
 		} 
-	 }   	  
-
+	}
+   	
 	while(j < qtd){
-		filho[k] = fork();
-		if (filho[k] == 0) {
-			(*numero) = primo(entrada[j]);
-			(*contador) = (*contador)+(*numero);
-			exit(0);
-		}
 
-		/* Verifica se atingiu o limite de processos permitidos */
-		if (k == 3){
-			for (i = 0; i < k; i++){
-				waitpid(filho[k], NULL, 0);
-				k = 0 ;     
+		for(k = 0; k < 4; k++){
+			if(libera[k] == 0){
+				filho[k] = fork();
+				libera[k] = 1;
+				
+				if(filho[k] == 0){
+					(*numero) = primo(entrada[j]);	
+					(*contador) = (*contador)+(*numero);
+
+					libera[k] = 0;
+					exit(0);
+				}
+				j++;
+				break;
 			}
 		}
-
-		k++;
-		j++;
 	}
-	for (i = 0; i < qtd; i++){
+
+	for (i = 0; i < 4; i++){
 	    waitpid(filho[i], NULL, 0);
 	}
-
+		
 	printf ("%d\n", (*contador)); 
-
+	
 	return 0;
-
+	
 }
