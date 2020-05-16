fdebd92082aa3a87997cc888b237ebcc497e6543
lg0017 <luizmb16@gmail.com>
2020-05-15 23:56:22 -0300

    Comentado e finalizado

--- src/main.c
+++ src/main.c
@@ -1,3 +1,8 @@
+/* Data de submissao: 15/05/2020
+ * Nome: Luiz Guilherme Silva Moreira
+ * RA: 202391
+ */
+
 #include <stdio.h>
 #include <stdlib.h> /* para utilizar exit */
 #include <sys/types.h> /* para utilizar pid_t */
@@ -25,8 +30,10 @@ int primo(int n){
 }
 	
 int main() {
+	/* Numero maximo de processos */
 	pid_t filho[4];
 
+	/* Variaveis de auxilio geral */
 	int entrada[100];
 	int qtd = 0, i = 0, j = 0, k = 0, saida = 0, inteiro;
 	char caracter;
@@ -35,20 +42,25 @@ int main() {
 	int protection = PROT_READ | PROT_WRITE;
 	int visibility = MAP_SHARED | MAP_ANON;
 
-	int *contador; 
+	/* Criar area de memoria compartilhada */
+	int *contador; /* Variavel contadora visivel para todos os processos */
 	contador = (int*) mmap(NULL, sizeof(int)*100, protection, visibility, 0, 0);
-	int *numero; 
+	int *numero; /* Variavel verificadora de primos visivel para todos os processos */
 	numero = (int*) mmap(NULL, sizeof(int)*100, protection, visibility, 0, 0);
-	int *libera; 
+	int *libera; /* Variavel controladora de processos visivel para todos os processos */
 	libera = (int*) mmap(NULL, sizeof(int)*4, protection, visibility, 0, 0);
 
+	/* Preenchendo o vetor liberador de zeros */
+	/* Legenda: 0 - processo livre 1 - processo ocupado */
 	for(i = 0; i < 4; i++){
 		libera[i] = 0;
 	}
 
 	i = 0;
-	
+
+	/* Tratamento da entrada */	
 	while (saida != -1){
+		/* Variavel caracter serve para ler espacos */
 		scanf("%d%c", &inteiro, &caracter);
 		if(caracter == '\n'){
 			entrada[i] = inteiro;
@@ -61,30 +73,39 @@ int main() {
 		} 
 	}
    	
+	/* Inicio dos processos para verificacao de primos */  
 	while(j < qtd){
 
+		/* Numero maximo de processos ao mesmo tempo */
 		for(k = 0; k < 4; k++){
+			/* Processo livre */
 			if(libera[k] == 0){
 				filho[k] = fork();
+				/* Processo ocupado */
 				libera[k] = 1;
 				
+				/* Estamos no filho */	
 				if(filho[k] == 0){
+					/* Verificacao e contagem */
 					(*numero) = primo(entrada[j]);	
 					(*contador) = (*contador)+(*numero);
-
+					/* Processo novamente livre */					
 					libera[k] = 0;
 					exit(0);
 				}
+				/* Anda no vetor de entradas */
 				j++;
 				break;
 			}
 		}
 	}
 
+	/* Aguarda todos os processos terminarem */
 	for (i = 0; i < 4; i++){
 	    waitpid(filho[i], NULL, 0);
 	}
-		
+	
+	/* Saida */	
 	printf ("%d\n", (*contador)); 
 	
 	return 0;
